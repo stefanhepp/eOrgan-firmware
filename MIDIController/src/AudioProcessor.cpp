45572fc69d9d0d88af3d3778bbf483633b07de93
Stefan Hepp <stefan@stefant.org>
2024-10-08 19:50:22 +0000

    Initial code for audio engine

---
+++ MIDIController/src/AudioProcessor.cpp
@@ -0,0 +1,50 @@
+
+/*
+ * @project     MIDIController
+ * @author      Stefan Hepp, stefan@stefant.org
+ *
+ * Audio effects and router
+ *
+ * Copyright 2024 Stefan Hepp
+ * License: GPL v3
+ * See 'COPYRIGHT.txt' for copyright and licensing information.
+ */
+#include "AudioEngine.h"
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioInputI2S2           i2s2_1;         //xy=88,97
+AudioInputUSB            usb1;           //xy=190,238
+AudioEffectReverb        reverb1;        //xy=261,108
+AudioMixer4              mixer2;         //xy=445,234
+AudioMixer4              mixer1;         //xy=449,133
+AudioOutputI2S2          i2s2_2;         //xy=636,179
+AudioConnection          patchCord1(i2s2_1, 0, reverb1, 0);
+AudioConnection          patchCord2(usb1, 0, mixer1, 1);
+AudioConnection          patchCord3(usb1, 1, mixer2, 1);
+AudioConnection          patchCord4(reverb1, 0, mixer1, 0);
+AudioConnection          patchCord5(reverb1, 0, mixer2, 0);
+AudioConnection          patchCord6(mixer2, 0, i2s2_2, 1);
+AudioConnection          patchCord7(mixer1, 0, i2s2_2, 0);
+AudioControlSGTL5000     sgtl5000_1;     //xy=359,376
+// GUItool: end automatically generated code
+
+AudioProcessor::AudioProcessor()
+{
+}
+
+void AudioProcessor::begin()
+{
+    AudioMemory(10);
+    sgtl5000_1.enable();
+    sgtl5000_1.volume(0.5);
+    mixer1.gain(0, 0.5);
+    mixer1.gain(1, 0.5);
+    mixer2.gain(0, 0.5);
+    mixer2.gain(1, 0.5);
+}
---
+++ MIDIController/src/AudioProcessor.h
@@ -0,0 +1,21 @@
+/**
+ * @project     MidiController
+ * @author      Stefan Hepp, stefan@stefant.org
+ *
+ * Audio effects and router
+ *
+ * Copyright 2024 Stefan Hepp
+ * License: GPL v3
+ * See 'COPYRIGHT.txt' for copyright and licensing information.
+ */
+#pragma once
+
+#include <inttypes.h>
+#include <common_config.h>
+
+class AudioProcessor {
+    public:
+        AudioProcessor();
+
+        void begin();
+};
