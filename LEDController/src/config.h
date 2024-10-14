aeba4ead0670d54b5b56a43481db85e8528548c7
Stefan Hepp <stefan@stefant.org>
2024-10-14 02:32:38 +0000

    Fix order of RGB pinout

--- LEDController/src/config.h
+++ LEDController/src/config.h
@@ -28,12 +28,12 @@ static const uint8_t BTN4        = 1;
 static const uint8_t BTN5        = 4;
 
 static const uint8_t PIN_INTERRUPT   = 0;
-static const uint8_t PIN_LED1_R      = 3;
-static const uint8_t PIN_LED1_G      = 2;
-static const uint8_t PIN_LED1_B      = 1;
-static const uint8_t PIN_LED2_R      = 7;
-static const uint8_t PIN_LED2_G      = 6;
-static const uint8_t PIN_LED2_B      = 5;
+static const uint8_t PIN_LED1_R      = 2;
+static const uint8_t PIN_LED1_G      = 1;
+static const uint8_t PIN_LED1_B      = 3;
+static const uint8_t PIN_LED2_R      = 6;
+static const uint8_t PIN_LED2_G      = 5;
+static const uint8_t PIN_LED2_B      = 7;
 static const uint8_t PIN_LED3        = 4;
 
 
